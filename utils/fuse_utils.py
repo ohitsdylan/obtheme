20e33cbdf41adbf45fb9ed433abbdb632244db97
Antonio Piraino <antonio_piraino@tiscali.it>
2015-04-11 00:07:51 +0200

    renamed file

--- utils/fuse_utils.py
+++ utils/fuse_utils.py
@@ -1,118 +1,136 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import absolute_import
+from __future__ import with_statement
 
 import os
-import time
+import sys
 import errno
-import fuse
-import stat
-from fuse import Fuse, Stat
+
+from fuse import FUSE, FuseOSError, Operations
 
 
-class MyStat(Stat):
+class Passthrough(Operations):
 
-    def __init__(self):
-        self.st_mode = stat.S_IFDIR | 0755
-        self.st_ino = 0
-        self.st_dev = 0
-        self.st_nlink = 2
-        self.st_uid = os.getuid()
-        self.st_gid = os.getgid()
-        self.st_size = 4096
-        self.st_atime = 0
-        self.st_mtime = 0
-        self.st_ctime = 0
+    def __init__(self, root):
+        self.root = root
 
+    # Helpers
+    # =======
 
-class SimpleDir(Fuse):
-    fuse.fuse_python_api = (0, 2)
+    def _full_path(self, partial):
+        if partial.startswith("/"):
+            partial = partial[1:]
+        path = os.path.join(self.root, partial)
+        return path
 
-    def __init__(self, *args, **kw):
-        Fuse.__init__(self, *args, **kw)
-        self.flags = 0
-        self.multithreaded = 0
-        self.files = {}
-        # self.files['themerc'] = ''
-
-    def getattr(self, path):
-        st = MyStat()
-        path.split('/')[1:]
-        st.st_atime = int(time())
-        st.st_mtime = st.st_atime
-        st.st_ctime = st.st_atime
-        if path == '/':
-            pass
-        elif path[1:] in self.files:
-            st.st_mode = stat.S_IFREG | 0666
-            st.st_nlink = 1
-            st.st_size = len(self.files[path[1:]])
-        else:
-            return -errno.ENOENT
-        return st
+    # Filesystem methods
+    # ==================
 
     def access(self, path, mode):
         full_path = self._full_path(path)
         if not os.access(full_path, mode):
-            print("BONK!")
-            raise ValueError(errno.EACCES)
+            raise FuseOSError(errno.EACCES)
 
-    def readdir(self, path, offset):
+    def chmod(self, path, mode):
+        full_path = self._full_path(path)
+        return os.chmod(full_path, mode)
+
+    def chown(self, path, uid, gid):
+        full_path = self._full_path(path)
+        return os.chown(full_path, uid, gid)
+
+    def getattr(self, path, fh=None):
+        full_path = self._full_path(path)
+        st = os.lstat(full_path)
+        return dict((key, getattr(st, key)) for key in (
+            'st_atime', 'st_ctime', 'st_gid', 'st_mode', 'st_mtime',
+            'st_nlink', 'st_size', 'st_uid'))
+
+    def readdir(self, path, fh):
+        full_path = self._full_path(path)
+
         dirents = ['.', '..']
-        if path == '/':
-            dirents.extend(self.files.keys())
+        if os.path.isdir(full_path):
+            dirents.extend(os.listdir(full_path))
         for r in dirents:
-            yield fuse.Direntry(r)
+            yield r
 
-    def read(self, path, size, offset):
-        if path[1:] in self.files:
-            return self.files[path[1:]][offset:offset+size]
+    def readlink(self, path):
+        pathname = os.readlink(self._full_path(path))
+        if pathname.startswith("/"):
+            # Path name is absolute, sanitize it.
+            return os.path.relpath(pathname, self.root)
         else:
-            return -errno.ENOENT
-
-    def write(self, path, buf, offset):
-        l = len(buf)
-        if path[1:] in self.files:
-            if offset == 0:
-                self.files[path[1:]] = ''
-            self.files[path[1:]] += buf
-            return l
-        else:
-            return -errno.ENOENT
+            return pathname
 
     def mknod(self, path, mode, dev):
-        if len(path) > 1:
-            self.files[path[1:]] = ''
-        return 0
-
-    def unlink(self, path):
-        if path[1:] in self.files:
-            del self.files[path[1:]]
-        return 0
-
-    def open(self, path, flags):
-        return 0
-
-    def truncate(self, path, size):
-        return 0
-
-    def chown(self, *args):
-        return 0
-
-    def utime(self, path, times):
-        return 0
-
-    def mkdir(self, path, mode):
-        return 0
+        return os.mknod(self._full_path(path), mode, dev)
 
     def rmdir(self, path):
-        return 0
+        full_path = self._full_path(path)
+        return os.rmdir(full_path)
 
-    def rename(self, pathfrom, pathto):
-        return 0
+    def mkdir(self, path, mode):
+        return os.mkdir(self._full_path(path), mode)
 
-    def fsync(self, path, isfsyncfile):
-        return 0
+    def statfs(self, path):
+        full_path = self._full_path(path)
+        stv = os.statvfs(full_path)
+        return dict((key, getattr(stv, key)) for key in (
+            'f_bavail', 'f_bfree', 'f_blocks', 'f_bsize', 'f_favail',
+            'f_ffree', 'f_files', 'f_flag', 'f_frsize', 'f_namemax'))
 
-    def release(self, path, flags):
-        return 0
+    def unlink(self, path):
+        return os.unlink(self._full_path(path))
+
+    def symlink(self, name, target):
+        return os.symlink(name, self._full_path(target))
+
+    def rename(self, old, new):
+        return os.rename(self._full_path(old), self._full_path(new))
+
+    def link(self, target, name):
+        return os.link(self._full_path(target), self._full_path(name))
+
+    def utimens(self, path, times=None):
+        return os.utime(self._full_path(path), times)
+
+    # File methods
+    # ============
+
+    def open(self, path, flags):
+        full_path = self._full_path(path)
+        return os.open(full_path, flags)
+
+    def create(self, path, mode, fi=None):
+        full_path = self._full_path(path)
+        return os.open(full_path, os.O_WRONLY | os.O_CREAT, mode)
+
+    def read(self, path, length, offset, fh):
+        os.lseek(fh, offset, os.SEEK_SET)
+        return os.read(fh, length)
+
+    def write(self, path, buf, offset, fh):
+        os.lseek(fh, offset, os.SEEK_SET)
+        return os.write(fh, buf)
+
+    def truncate(self, path, length, fh=None):
+        full_path = self._full_path(path)
+        with open(full_path, 'r+') as f:
+            f.truncate(length)
+
+    def flush(self, path, fh):
+        return os.fsync(fh)
+
+    def release(self, path, fh):
+        return os.close(fh)
+
+    def fsync(self, path, fdatasync, fh):
+        return self.flush(path, fh)
+
+
+def main(mountpoint, root):
+    FUSE(Passthrough(root), mountpoint, foreground=True)
+
+if __name__ == '__main__':
+    main(sys.argv[2], sys.argv[1])

66c65b2f7afb2bf6a77379e047347920afd83fbc
Antonio Piraino <antonio_piraino@tiscali.it>
2015-04-11 02:23:46 +0200

    removed unused function

--- utils/general_utils.py
+++ utils/general_utils.py
@@ -64,21 +64,6 @@ def write_file(path, contents):
         return True
 
 
-def which(program):
-    def is_exe(fpath):
-        return os.path.exists(fpath) and os.access(fpath, os.X_OK)
-    fpath, fname = os.path.split(program)
-    if fpath:
-        if is_exe(program):
-            return program
-    else:
-        for path in os.environ["PATH"].split(os.pathsep):
-            exe_file = os.path.join(path, program)
-            if is_exe(exe_file):
-                return exe_file
-    return None
-
-
 def clear_dir(dpath):
     if os.path.exists(dpath):
         for item in os.listdir(dpath):
